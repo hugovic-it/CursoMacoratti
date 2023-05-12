05908602c378474195c783574cd516a85f8587dc
Hugo Victor <hugovic.it@gmail.com>
2023-05-12 08:36:22 -0300

    37. populando a tabela categorias

---
+++ APICatalogo/Migrations/20230512113303_PopulaCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Categorias(Nome, imagemUrl) Values('Bebidas','bebidas.jpg')");
+            mb.Sql("Insert into Categorias(Nome, imagemUrl) Values('Lanches','lanches.jpg')");
+            mb.Sql("Insert into Categorias(Nome, imagemUrl) Values('Sobremesas','sobremesas.jpg')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Categorias");
+        }
+    }
+}
