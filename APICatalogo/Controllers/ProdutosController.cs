bb129a435b329967dfe7ea7866ab1a73c8359f01
Hugo Victor <hugovic.it@gmail.com>
2023-05-22 11:45:24 -0300

    44. ProdutosControler - Put: Atualiza um produto

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -2,53 +2,70 @@
 using APICatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace APICatalogo.Controllers
+namespace APICatalogo.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class ProdutosController : ControllerBase
 {
-    [Route("[controller]")]
-    [ApiController]
-    public class ProdutosController : ControllerBase
+    private readonly AppDbContext _context;
+    public ProdutosController(AppDbContext context)
     {
-        private readonly AppDbContext _context;
-        public ProdutosController(AppDbContext context)
-        {
-            _context = context;
-        }
-
-        [HttpGet]
-        public ActionResult<IEnumerable<Produto>> Get() 
-        { 
-            var produtos = _context.Produtos.ToList();
-            if(produtos is null)
-            {
-                return NotFound("Produtos não encontrados...");
-            }
-            return produtos;
-        }
-
-
-        [HttpGet("{id}", Name="ObterProduto")]
-        public ActionResult<Produto> Get(int id) 
-        {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (produto is null)
-            {
-                return NotFound("Produto não encontrado...");
-            }
-            return produto;
-        }
-
-        [HttpPost]
-        public ActionResult<Produto> Get(Produto produto) 
-        {
-            if(produto is null)
-            {
-                BadRequest();
-            }
-
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
-            return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
-        }
+        _context = context;
     }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<Produto>> Get()
+    {
+        var produtos = _context.Produtos.ToList();
+        if (produtos is null)
+        {
+            return NotFound("Produtos não encontrados...");
+        }
+        return produtos;
+    }
+
+
+    [HttpGet("{id}", Name = "ObterProduto")]
+    public ActionResult<Produto> Get(int id)
+    {
+        var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+        if (produto is null)
+        {
+            return NotFound("Produto não encontrado...");
+        }
+        return produto;
+    }
+
+    [HttpPost]
+    public ActionResult<Produto> Get(Produto produto)
+    {
+        if (produto is null)
+        {
+            BadRequest();
+        }
+
+        _context.Produtos.Add(produto);
+        _context.SaveChanges();
+        return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+    }
+
+    [HttpPut("{id:int}")]
+    public ActionResult Put(int id, Produto produto)
+    {
+        if(id != produto.ProdutoId)
+        {
+            return BadRequest();
+        }
+        _context.Entry(produto).State = EntityState.Modified;
+        _context.SaveChanges();
+
+        return Ok(produto);
+    }
+
+    
+        
+    
 }
