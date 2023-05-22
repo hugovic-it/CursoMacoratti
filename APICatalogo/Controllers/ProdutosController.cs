1cfadb14ef0446230c813016c989505b42fedab6
Hugo Victor <hugovic.it@gmail.com>
2023-05-22 11:26:39 -0300

    42. ProdutosController - Get: Retorna produto por ID

--- APICatalogo/Controllers/ProdutosController.cs
+++ APICatalogo/Controllers/ProdutosController.cs
@@ -26,5 +26,16 @@ namespace APICatalogo.Controllers
             return produtos;
         }
 
+
+        [HttpGet("{id}")]
+        public ActionResult<Produto> Get(int id) 
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if (produto is null)
+            {
+                return NotFound("Produto não encontrado...");
+            }
+            return produto;
+        }
     }
 }
